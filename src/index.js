0569a486b852349ee63ebd72679c414a5f7f1709
Abhishek <125677567+Abhishek6827@users.noreply.github.com>
2025-08-29 07:42:10 +0530

    Ji

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {configureStore} from '@reduxjs/toolkit'
-import {BrowserRouter} from 'react-router-dom'
-import {Provider} from 'react-redux'
-import Article from './Component/Reducers/ArticleReducer'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import Article from "./Component/Reducers/ArticleReducer";
 
-const store= configureStore({
-  reducer:{
-    Blogs:Article,
-  }
-})
+const store = configureStore({
+  reducer: {
+    Blogs: Article,
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter>
-  <Provider store={store}>
-  <App />
-  </Provider>
-  </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
